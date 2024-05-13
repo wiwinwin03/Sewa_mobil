4562a6b207ab01c219568a04ca6b071bf58fae5f
wiwinwin03 <wiwinazhagent@gmail.com>
2024-05-13 13:40:01 +0700

    first commit

---
+++ app/Http/Controllers/Admin/typeController.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\type;
+use Illuminate\Support\Str;
+use Yajra\DataTables\DataTables;
+use App\Http\Requests\typeRequest;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class typeController extends Controller
+{
+    /** 
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response|\Illuminate\Http\JsonResponse|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
+    public function index()
+    {
+        if (request()->ajax()) {
+            $query = type::query();
+
+            return DataTables::of($query)
+                ->addColumn('action', function ($type) {
+                    return '
+                    <a class="block w-full px-2 py-1 mb-1 text-xs text-center text-white transition duration-500 bg-gray-700 border border-gray-700 rounded-md select-none ease hover:bg-gray-800 focus:outline-none focus:shadow-outline" 
+                        href="' . route('admin.types.edit', $type->id) . '">
+                        Sunting
+                    </a>
+                    <form class="block w-full" onsubmit="return confirm(\'Apakah anda yakin?\');" -block" action="' . route('admin.types.destroy', $type->id) . '" method="POST">
+                    <button class="w-full px-2 py-1 text-xs text-white transition duration-500 bg-red-500 border border-red-500 rounded-md select-none ease hover:bg-red-600 focus:outline-none focus:shadow-outline" >
+                        Hapus
+                    </button>
+                        ' . method_field('delete') . csrf_field() . '
+                    </form>';
+                })
+                ->rawColumns(['action'])
+                ->make();
+        }
+
+        return view('admin.type.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View returned
+     */
+    public function create()
+    {
+        return view('admin.type.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(typeRequest $request)
+    {
+        $data = $request->all();
+        $data['slug'] = Str::slug($data['name']) . '-' . Str::lower(Str::random(5));
+
+        type::create($data);
+
+        return redirect()->route('admin.types.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\type  $type
+     * @return \Illuminate\Http\Response|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
+    public function edit(type $type)
+    {
+        return view('admin.type.edit', [
+            'type' => $type,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\type  $type
+     * @return \Illuminate\Http\Response
+     */
+    public function update(typeRequest $request, type $type)
+    {
+        $data = $request->all();
+        $data['slug'] = Str::slug($data['name']) . '-' . Str::lower(Str::random(5));
+
+        $type->update($data);
+
+        return redirect()->route('admin.types.index');
+    }
+
+   /**
+ * Remove the specified resource from storage.
+ *
+ * @param  \App\Models\type  $type
+ * @return \Illuminate\Http\Response
+ */
+public function destroy(type $type)
+{
+    $type->delete();
+
+    return redirect()->route('admin.types.index');
+}
+}
